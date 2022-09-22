b6ef3e54342eedfe271b8490090561d2b8e38f5c
manojvs2002 <106401589+manojvs2002@users.noreply.github.com>
2022-09-22 21:26:07 +0530

    Add files via upload

---
+++ first.java
@@ -0,0 +1,11 @@
+import java.lang.Math;
+class squareroot
+{
+    public static void main(String args[])
+    {
+        double x=4,y;
+        y=Math.sqrt(x);
+        System.out.println("y="+y);
+
+    }
+}
