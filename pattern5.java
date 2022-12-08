a1d0997546b65da8d88293eda53069b41694cd7a
manojvs2002 <106401589+manojvs2002@users.noreply.github.com>
2022-12-08 20:13:15 +0530

    Add files via upload

---
+++ pattern5.java
@@ -0,0 +1,29 @@
+package manoj;
+
+public class pattern5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n=14;
+		for(int i=0;i<n;i++)
+		{
+			for(int j=0;j<n;j++)
+			{
+				
+				if(i==(n-1)/2 && j>=(n-1)/4 && j<=(3*n)/4 || i==0 && j>=(n-1)/4 && j<=(3*n)/4 || j==(n-1)/4 || j==(3*n)/4 )
+				{
+				System.out.print("*");
+				}
+				else
+				{
+				System.out.print(" ");
+				}
+				
+			}
+			System.out.println();
+
+	}
+
+	}
+
+}
